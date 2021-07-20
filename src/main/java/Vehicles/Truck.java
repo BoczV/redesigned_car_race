9a08b1c95bcda1648de1c2404a0c335d584151ff
Viktor <beviktor95@gmail.com>
2021-07-20 22:29:35 +0200

    I've finished Truck class as well

--- src/main/java/Vehicles/Truck.java
+++ src/main/java/Vehicles/Truck.java
@@ -1,10 +1,34 @@
 package Vehicles;
 
+import race.Race;
+
 public class Truck extends Vehicle {
+    private boolean brokenDown;
+    private int brokenDownCounter;
+
 
     @Override
     protected void prepareForLap() {
+        if(brokenDown){
+            if(brokenDownCounter == 1) brokenDownCounter += 1;
+            else {
+                brokenDown = false;
+                brokenDownCounter = 0;
+                Race.Circumstances.setTruckBroken(false);
+            }
+        }
+        doIBreakDown();
+        this.normalSpeed = 100;
+        this.actualSpeed = brokenDown? 0: 100;
+    }
 
+    private void doIBreakDown(){
+        int chanceOfBreakingDown = 1 + random.nextInt(100);
+        brokenDown = chanceOfBreakingDown <= 5;
+        if (brokenDown) {
+            Race.Circumstances.setTruckBroken(true);
+            brokenDownCounter = 1;
+        }
     }
 
     @Override
