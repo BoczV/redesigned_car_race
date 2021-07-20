c1aaecc5a1f28b6834d6de9876919e029ce3b8c4
Viktor <beviktor95@gmail.com>
2021-07-20 21:33:21 +0200

    Elements of Car class has been filled

--- src/main/java/Vehicles/Car.java
+++ src/main/java/Vehicles/Car.java
@@ -2,27 +2,27 @@ package Vehicles;
 
 import race.Race;
 
-public class Car extends  Vehicle {
-
-
+public class Car extends Vehicle {
 
     @Override
     public void moveForAnHour() {
-
+        prepareForLap();
+        this.distanceTraveled += actualSpeed;
     }
 
     @Override
     protected void prepareForLap() {
-
+        this.normalSpeed = 80 + random.nextInt(31);
+        this.actualSpeed = Race.Circumstances.isTruckBroken()? 75: normalSpeed;
     }
 
     @Override
     public int getDistance() {
-        return 0;
+        return this.distanceTraveled;
     }
 
     @Override
     public String getType() {
-        return null;
+        return "Car";
     }
 }
