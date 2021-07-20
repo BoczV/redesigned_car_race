d6f0559dc7fb7644f263d5dce809e5f48262e845
Viktor <beviktor95@gmail.com>
2021-07-20 22:36:30 +0200

    Setup Main to run project

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,21 @@
 import Vehicles.Car;
+import Vehicles.Motorcycle;
+import Vehicles.Truck;
+import Vehicles.Vehicle;
+import race.Race;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello project");
+        int numberOfRacer = 30;
+        Race race = new Race(numberOfRacer, 50);
+        for(int i = 0; i < numberOfRacer; i++){
+            Vehicle vehicle = i < 10? new Car(): i < 20 ? new Motorcycle(): new Truck();
+            race.registerRacer(vehicle, i);
+        }
 
+        race.simulateRace();
+        race.printRaceResults();
     }
 }
